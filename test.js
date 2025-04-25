2a64c7b93b52d9ec02e87b8012b20f3f1c76acbd
reginaldo.lopes <reginaldo.lopes@unesp.br>
2025-04-25 11:38:22 -0300

    test

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const app = require("./index.js");
 
+
 describe("GET /", () => {
   test("O / deve ser acessível", async () => {
     const response = await request(app).get("/");
